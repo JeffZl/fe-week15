1a721842891cb72213bd73c8ccac755cfa73de90
JeffZl <86556813+JeffZl@users.noreply.github.com>
2025-11-24 11:09:17 +0700

    database

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,29 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET(
+    _: Request, { params }: { params: Promise<{id: string}>}
+) {
+    const { id } = await params
+    const post = await prisma.post.findUnique({
+        where: {
+            id: Number(id)
+        }
+    })
+
+    if(!post) return NextResponse.json({ error: "Not Found" }, { status: 404 })
+    return NextResponse.json(post)
+}
+
+export async function DELETE(
+    _: Request, { params }: { params: Promise<{id: string}>}
+) {
+    const { id } = await params
+    await prisma.post.delete({
+        where: {
+            id: Number(id)
+        }
+    })
+
+    return NextResponse.json({ message: "deleted" }, { status: 204 })
+}
